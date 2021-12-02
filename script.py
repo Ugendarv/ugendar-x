7fdde0b3a359b699e9c16a368d329ad85f77e812
Ugendarv <86180392+Ugendarv@users.noreply.github.com>
2021-12-02 06:22:40 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,7 +12,7 @@ Just Add me to Your Group As Admin, Hit The Help Button For More Info..</b>"""
 <b>Here Is The Help For My Commands.</b>"""
 
     ABOUT_TXT = """<b>➥ My name: {}
-➥ Creator: Zaute Km
+➥ Creator: ugendar v
 ➥ Library: Pyrogram
 ➥ Language: Python 𝟹
 ➥ Data Base: MongoDB
@@ -21,7 +21,7 @@ Just Add me to Your Group As Admin, Hit The Help Button For More Info..</b>"""
 
     SOURCE_TXT = """<b>Source:</b>
 IMDb is a Open source project.
-Source: <a href='https://github.com/josprojects/tgmoviebot'>GitHub - Click here 👈</a>
+Source: <a href='https://github.com/Ugendarv/ugendar-x'>GitHub - Click here 👈</a>
 
 <b>DEVS:</b>
 - <a href='https://t.me/josprojects'>Jos Projects</a>
